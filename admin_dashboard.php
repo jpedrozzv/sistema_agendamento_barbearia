7c304c05e07f2974d3ad46333e1c69a881237d1a
joao pedro amaral <142542244+jpedrozzv@users.noreply.github.com>
2025-11-09 02:03:37 -0300

    Padroniza cards do painel do administrador

--- admin_dashboard.php
+++ admin_dashboard.php
@@ -2,29 +2,45 @@
 
 <h2 class="text-center mb-4">💈 Painel do Administrador</h2>
 
-<div class="row text-center g-3">
-  <div class="col-md-3">
-    <a href="listar_agendamentos_adm.php" class="btn btn-outline-dark w-100 p-4 shadow-sm">
-      <i class="bi bi-calendar3 fs-2 mb-2"></i><br>Gerenciar Agendamentos
-    </a>
+<div class="row row-cols-1 row-cols-sm-2 row-cols-lg-4 g-4">
+  <div class="col">
+    <div class="card h-100 text-center shadow-sm">
+      <div class="card-body d-flex flex-column align-items-center">
+        <i class="bi bi-calendar3 display-6 mb-3 text-secondary"></i>
+        <h5 class="card-title">Gerenciar Agendamentos</h5>
+        <a href="listar_agendamentos_adm.php" class="btn btn-outline-dark mt-auto w-100">Acessar</a>
+      </div>
+    </div>
   </div>
 
-  <div class="col-md-3">
-    <a href="listar_clientes.php" class="btn btn-outline-dark w-100 p-4 shadow-sm">
-      <i class="bi bi-people fs-2 mb-2"></i><br>Gerenciar Clientes
-    </a>
+  <div class="col">
+    <div class="card h-100 text-center shadow-sm">
+      <div class="card-body d-flex flex-column align-items-center">
+        <i class="bi bi-people display-6 mb-3 text-secondary"></i>
+        <h5 class="card-title">Gerenciar Clientes</h5>
+        <a href="listar_clientes.php" class="btn btn-outline-dark mt-auto w-100">Acessar</a>
+      </div>
+    </div>
   </div>
 
-  <div class="col-md-3">
-    <a href="listar_servicos.php" class="btn btn-outline-dark w-100 p-4 shadow-sm">
-      <i class="bi bi-scissors fs-2 mb-2"></i><br>Gerenciar Serviços
-    </a>
+  <div class="col">
+    <div class="card h-100 text-center shadow-sm">
+      <div class="card-body d-flex flex-column align-items-center">
+        <i class="bi bi-scissors display-6 mb-3 text-secondary"></i>
+        <h5 class="card-title">Gerenciar Serviços</h5>
+        <a href="listar_servicos.php" class="btn btn-outline-dark mt-auto w-100">Acessar</a>
+      </div>
+    </div>
   </div>
 
-  <div class="col-md-3">
-    <a href="gerenciar_feriados.php" class="btn btn-outline-dark w-100 p-4 shadow-sm">
-      <i class="bi bi-calendar-x fs-2 mb-2"></i><br>Gerenciar Feriados
-    </a>
+  <div class="col">
+    <div class="card h-100 text-center shadow-sm">
+      <div class="card-body d-flex flex-column align-items-center">
+        <i class="bi bi-calendar-x display-6 mb-3 text-secondary"></i>
+        <h5 class="card-title">Gerenciar Feriados</h5>
+        <a href="gerenciar_feriados.php" class="btn btn-outline-dark mt-auto w-100">Acessar</a>
+      </div>
+    </div>
   </div>
 </div>
 
